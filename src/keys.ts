e97cd0fbb7274bf0ef178a384f2323a2dd6e130c
Alberto Benatti <albi.benni8@gmail.com>
2025-08-08 14:28:20 +0200

    array and obj

---
+++ src/keys.ts
@@ -0,0 +1,72 @@
+import { createBenchmark } from "./benchmark";
+
+class MyArray extends Array {}
+
+const SIZE = 100;
+const obj: Record<string, number> = {};
+/**
+ *  {
+ *  _0: 0,
+ *  _1: 1,
+ *  _2: 2,
+ *  ...
+ * }
+ */
+const array = [];
+/**
+ *  [
+ *  '_0', 0,
+ *  '_1', 1,
+ *  '_2', 2,
+ *  ...
+ * }
+ */
+
+for (let i = 0; i < SIZE; i++) {
+  obj["_" + i] = i;
+  // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+  //@ts-ignore
+  array.push("_" + i, i);
+}
+
+(function doBenchmarks(benchmark) {
+  (function benchmark1(obj, timer) {
+    let sum = 0;
+    while (timer()) {
+      for (const key in obj) {
+        sum += obj[key];
+      }
+    }
+  })(obj, benchmark("property"));
+
+  (function benchmark1(obj, timer) {
+    let sum = 0;
+    while (timer()) {
+      const copy = { ...obj };
+      for (const key in copy) {
+        sum += copy[key];
+      }
+    }
+  })(obj, benchmark("property with copy"));
+
+  (function benchmark2(array, timer) {
+    let sum = 0;
+    while (timer()) {
+      for (let i = 0; i < array.length; i += 2) {
+        sum += array[i] as number;
+      }
+    }
+  })(array, benchmark("array"));
+
+  (function benchmark2(array, timer) {
+    let sum = 0;
+    while (timer()) {
+      const copy = array.slice();
+      for (let i = 0; i < copy.length; i += 2) {
+        sum += copy[i] as number;
+      }
+    }
+  })(array, benchmark("array with copy"));
+
+  benchmark.report();
+})(createBenchmark("iteration"));
