8cd7db8f289aba5fa33ae626672997732dcbddf8
Alberto Benatti <albi.benni8@gmail.com>
2025-08-08 14:15:28 +0200

    equality

---
+++ src/equality.ts
@@ -0,0 +1,38 @@
+import { createBenchmark } from "./benchmark";
+
+const objs: any[] = [];
+Object.prototype.valueOf = function (this: { value: any }) {
+  // eslint-disable-next-line @typescript-eslint/no-unsafe-return
+  return this.value;
+};
+
+for (let i = 0; i < 1000; i++) {
+  objs.push({
+    ["_" + i]: i,
+    value: i,
+    valueOf: () => i,
+  });
+}
+
+(function doBenchmarks(benchmark) {
+  (function benchmark1(timer) {
+    let sum = false;
+    while (timer()) {
+      for (let i = 0; i < objs.length; i++) {
+        sum = objs[i] === i || sum;
+        // if (sum) throw new Error();
+      }
+    }
+  })(benchmark("==="));
+
+  (function benchmark2(timer) {
+    let sum = false;
+    while (timer()) {
+      for (let i = 0; i < objs.length; i++) {
+        sum = objs[i] == i || sum;
+        // if (sum) throw new Error();
+      }
+    }
+  })(benchmark("=="));
+  benchmark.report();
+})(createBenchmark("numbers"));
